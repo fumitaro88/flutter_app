01e803b7c506137ff8f14bec55b40df522c3da58
fumiya m <kali.karnel080@gmail.com>
2022-01-24 11:47:20 +0900

    test commit

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'FultterApp!'),
+      home: const MyHomePage(title: 'Fultter!'),
     );
   }
 }
